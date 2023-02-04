8ff0c32b73226e3261f586607748fa5bbd465282
Mammad N. Mammadli <mammadnm@code.local>
2023-02-04 12:59:24 +0400

    Codemaster

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region task1
+
+
+
+            ////number:
+            ////    Console.WriteLine("number");
+            ////    int number = int.Parse(Console.ReadLine());
+
+
+
+            ////    int countofDigit = (int)Math.Log10(number) + 1;
+
+            ////    if (countofDigit == 5)
+
+            ////    {
+            ////        int a = 4 * 1000000;
+            ////        int b = number * 10;
+            ////        int c = 2;
+            ////        int result = a + b + c;
+            ////        Console.WriteLine(result);
+            ////    }
+            ////    else
+            ////    {
+            ////        Console.WriteLine("eded 5 reqemli deyil");
+
+            ////        goto number;
+            ////    }
+
+            #endregion
+
+
+
+            #region task 2
+
+
+            ////age:
+
+            ////    Console.WriteLine("age");
+            ////    int age = int.Parse(Console.ReadLine());
+
+
+
+            ////    if (1 <= age && age <= 1888)
+            ////    {
+            ////        int year = age / 12;
+            ////        int month = age % 12;
+            ////        Console.WriteLine($"{year} il {month} ay");
+
+            ////    }
+
+            ////    else
+            ////    {
+            ////        Console.WriteLine("eded verilen araliqa uygun deyil");
+
+            ////        goto age;
+
+
+            ////    }
+            #endregion
+
+
+
+            ///hello 
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
